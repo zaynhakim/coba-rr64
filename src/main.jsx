7fa7772be903e1f98d24638a122e979793f5e565
zaynhakim <zaynhakim@gmail.com>
2022-10-24 12:03:46 +0700

    minor fixes

--- src/main.jsx
+++ src/main.jsx
@@ -23,30 +23,31 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      { index: true, element: <Index /> },
       {
-        errorElement: <ErrorPage />,
-        children: [
-          { index: true, element: <Index /> },
-          {
-            path: "contacts/:contactId",
-            element: <Contact />,
-            loader: contactLoader,
-            action: contactAction,
-          },
-          {
-            path: "contacts/:contactId/edit",
-            element: <EditContact />,
-            loader: contactLoader,
-            action: editAction,
-          },
-          {
-            path: "contacts/:contactId/destroy",
-            action: destroyAction,
-            errorElement: <div>Gak boleh woy!!</div>,
-          },
-        ],
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+        action: contactAction,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Gak boleh woy!!</div>,
       },
     ],
+    // children: [
+    //   {
+    //     errorElement: <ErrorPage />,
+
+    //   },
+    // ],
   },
 ]);
 
--- src/routes/destroy.jsx
+++ src/routes/destroy.jsx
@@ -2,7 +2,7 @@ import { redirect } from "react-router-dom";
 import { deleteContact } from "../contacts";
 
 export async function action({ params }) {
-  throw new Error("anjayy!");
+  // throw new Error("anjayy!");
   await deleteContact(params.contactId);
   return redirect("/");
 }
